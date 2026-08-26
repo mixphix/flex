bf520cb2d7c31ee5926e3a811246609fdf696117
Melanie Brown <brown.m@pm.me>
2026-08-26 16:04:27 -0400

    instance Tabulation Minkowski

--- lib/Flex/Math/Minkowski.hs
+++ lib/Flex/Math/Minkowski.hs
@@ -1,21 +1,27 @@
+{-# LANGUAGE NoFieldSelectors #-}
+
 module Flex.Math.Minkowski
-  ( Minkowski (Minkowski)
+  ( Minkowski (Minkowski, t, x, y, z)
+  , MinkowskiBasis (..)
   , Scalar (..)
   )
   where
 
+import Flex.Math.Category
 import Flex.Math.Module
 import Flex.Math.Numbers
 
 import Data.Foldable qualified as Data
 import Data.Functor qualified as Data
+import Data.Monoid (Monoid)
+import Data.Semigroup (Semigroup ((<>)))
 import Data.Traversable qualified as Data
 import Data.Eq (Eq)
 import Data.Ord (Ord)
 import Text.Show (Show)
 import GHC.Generics (Generic)
 
-data Minkowski x = Minkowski !x !x !x !x
+data Minkowski x = Minkowski {t :: !x, x :: !x, y :: !x, z :: !x}
   deriving
     ( Eq
     , Ord
@@ -25,6 +31,31 @@ data Minkowski x = Minkowski !x !x !x !x
     , Data.Traversable
     , Generic
     )
+
+instance Morphisms (->) (->) Minkowski where
+  morphism :: (x -> y) -> Minkowski x -> Minkowski y
+  morphism = Data.fmap
+instance Folds (->) (->) Minkowski where
+  foldWith :: (Monoid z) => (x -> z) -> Minkowski x -> z
+  foldWith f (Minkowski t x y z) = f t <> f x <> f y <> f z
+instance Traversals (->) (->) Minkowski where
+  traverse :: (Applicative g) => (x -> g y) -> Minkowski x -> g (Minkowski y)
+  traverse f (Minkowski t x y z) =
+    pure Minkowski <*> f t <*> f x <*> f y <*> f z
+instance Pure Minkowski where
+  pure :: x -> Minkowski x
+  pure x = Minkowski x x x x
+instance Apply Minkowski where
+  liftA2 :: (x -> y -> z) -> Minkowski x -> Minkowski y -> Minkowski z
+  liftA2 f (Minkowski t0 x0 y0 z0) (Minkowski t1 x1 y1 z1) =
+    Minkowski (f t0 t1) (f x0 x1) (f y0 y1) (f z0 z1)
+
+data MinkowskiBasis
+  = T
+  | X
+  | Y
+  | Z
+  deriving (Eq, Ord, Show, Generic)
 
 instance (Addition x x x) => Addition (Minkowski x) (Minkowski x) (Minkowski x) where
   (+.) :: Minkowski x -> Minkowski x -> Minkowski x
@@ -51,7 +82,21 @@ instance (Multiplication x x x) => Multiplication (Minkowski x) x (Minkowski x) 
 instance (Division x x x) => Division (Minkowski x) x (Minkowski x) where
   (/.) :: Minkowski x -> x -> Minkowski x
   Minkowski t x y z /. k = Minkowski (t /. k) (x /. k) (y /. k) (z /. k)
-  
+
+instance Collectable Minkowski where
+  distribute :: (Along f) => f (Minkowski x) -> Minkowski (f x)
+  distribute fm = Minkowski (morphism (.t) fm) (morphism (.x) fm) (morphism (.y) fm) (morphism (.z) fm)
+instance Tabulation Minkowski where
+  type Table Minkowski = MinkowskiBasis
+  fromTable :: (Table Minkowski -> x) -> Minkowski x
+  fromTable f = Minkowski (f T) (f X) (f Y) (f Z)
+  toTable :: Minkowski x -> Table Minkowski -> x
+  toTable (Minkowski t x y z) = \case
+    T -> t
+    X -> x
+    Y -> y
+    Z -> z
+
 instance (Addition x x x) => Addition (Scalar (Minkowski x)) (Scalar (Minkowski x)) (Scalar (Minkowski x)) where
   (+.) :: Scalar (Minkowski x) -> Scalar (Minkowski x) -> Scalar (Minkowski x)
   ScalarMinkowski x +. ScalarMinkowski y = ScalarMinkowski (x + y)
